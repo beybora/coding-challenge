f122505d8825c193b7d251dfe2f22a847053fd35
beybora <bora.b.sarikaya@gmail.com>
2025-07-08 19:29:06 +0200

    feat: render paginated pages + demo HTML

---
+++ src/components/v2/PaginationEditor.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="flex flex-col min-h-screen bg-gray-100">
+    <!-- Toolbar -->
+    <EditorToolbar :pageHeight="pageHeight" @update:pageHeight="setPageHeight" />
+
+    <!-- Page Wrapper -->
+    <div ref="wrapperRef"
+         class="flex flex-col items-center gap-14 overflow-hidden py-8"></div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import EditorToolbar from './EditorToolbar.vue'
+import { usePagination } from '../../composables/usePagination'
+
+// put the whole block in one template literal
+const demoHtml = /* html */ `
+  <!-- Bild ganz oben -->
+  <img src="https://picsum.photos/600/200" class="my-4" />
+
+  <b>This is the heading</b>
+
+
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+  <!-- Bild in der Mitte -->
+  <img src="https://picsum.photos/600/200" class="my-4" />
+
+  <table class="my-4" border="1">
+    <tr><td>Cell&nbsp;1</td><td>Cell&nbsp;2</td></tr>
+    <tr><td>Cell&nbsp;3</td><td>Cell&nbsp;4</td></tr>
+    <tr><td>Cell&nbsp;1</td><td>Cell&nbsp;2</td></tr>
+    <tr><td>Cell&nbsp;3</td><td>Cell&nbsp;4</td></tr>
+    <tr><td>Cell&nbsp;1</td><td>Cell&nbsp;2</td></tr>
+    <tr><td>Cell&nbsp;3</td><td>Cell&nbsp;4</td></tr>
+    <tr><td>Cell&nbsp;1</td><td>Cell&nbsp;2</td></tr>
+    <tr><td>Cell&nbsp;3</td><td>Cell&nbsp;4</td></tr>
+  </table>
+
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+  <ul>
+    <li>List item</li>
+    <li>another</li>
+    <li>item</li>
+    <li>item</li>
+    <li>item</li>
+    <li>item</li>
+    <li>item</li>
+    <li>item</li>
+  </ul>
+
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+  <!-- Bild ganz unten -->
+  <img src="https://picsum.photos/600/200" class="my-4" />
+
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+`;
+
+
+// initialise pagination with the dummy
+const { wrapperRef, pageHeight, setPageHeight } = usePagination(demoHtml)
+</script>
