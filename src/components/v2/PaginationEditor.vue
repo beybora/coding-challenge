332c6087b0a36b2040188845eb522f1e61778d8a
beybora <bora.b.sarikaya@gmail.com>
2025-07-08 19:41:28 +0200

    style: add basic typography for headings, lists & tables

--- src/components/v2/PaginationEditor.vue
+++ src/components/v2/PaginationEditor.vue
@@ -13,12 +13,11 @@
 import EditorToolbar from './EditorToolbar.vue'
 import { usePagination } from '../../composables/usePagination'
 
-// put the whole block in one template literal
-const demoHtml = /* html */ `
+const demoHtml = `
   <!-- Bild ganz oben -->
   <img src="https://picsum.photos/600/200" class="my-4" />
 
-  <b>This is the heading</b>
+  <h1>This is the heading</h1>
 
 
   <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
